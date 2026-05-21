14dbee46c07f96e49c34b44dbe804fdd167848b9
sydxis <k01023100681@gmail.com>
2026-05-21 22:45:42 +0900

    아이템 및 인벤토리 기반완성
    Inventory.h/.cpp를 도입하여 아이템 컨테이너 및 사용 기능을 관리하도록 구현했습니다.
    또한 HP_Potion.h와 MP_Potion.h를 추가해, Item을 상속받는 사용 가능한 아이템으로 만들고 각각 효과 수치와 Use 구현을 포함했습니다.
    
    Item.h 수정사항
    -ItemTypeEnum과 Player 추가
    -Type 및 EffectAmount 멤버 변수 추가
    -printInfo 헬퍼 함수 추가
    -가상 함수 Use 추가
    
    더 이상 필요 없는 Item.cpp와 ItemDB.h는 제거했습니다.

---
+++ TEXT_RPG_5/TEXT_RPG_5/HP_Potion.h
@@ -0,0 +1,23 @@
+﻿//HP_Potion.h
+#pragma once
+
+#include "Item.h"
+
+struct HP_Potion : public Item
+{
+	HP_Potion()
+	{
+		Name = "HP포션";
+		Type = ItemType::Usable;
+		Price = 50;
+		EffectAmount = 50;
+	}
+
+	void Use(Player* player) override
+	{
+		Item::Use(player);
+
+		cout << "HP + " << EffectAmount << "\n";
+
+	}
+};
---
+++ TEXT_RPG_5/TEXT_RPG_5/Inventory.h
@@ -0,0 +1,25 @@
+﻿//Inventory.h
+
+#pragma once
+
+#include "pch.h"
+#include "Item.h"
+
+class Inventory
+{
+protected:
+	vector<Item*> Container;
+	Player* Player;
+
+
+public:
+	void printInventory();
+	void AddItem(Item* item);
+
+	bool RemoveItem(Item* item);
+	bool UseItem(Item* item);
+	
+
+
+};
+
