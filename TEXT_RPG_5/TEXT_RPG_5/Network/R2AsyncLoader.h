d31f61cba8ac92eaa3f2901f6d5c0a8c4fe30995
dltmdwo2323@gmail.com <sky_battle@naver.com>
2026-05-28 20:58:04 +0900

    R2 Storage 셋팅

---
+++ TEXT_RPG_5/TEXT_RPG_5/Network/R2AsyncLoader.h
@@ -0,0 +1,49 @@
+﻿# pragma once
+#include <future>
+
+#include "../pch.h"
+#include "../Struct/R2Config.h"
+#include "../Struct/R2Result.h"
+
+namespace Aws::S3
+{
+	class S3Client;
+}
+
+class R2AsyncLoader
+{
+public:
+	void StartLoadIni(
+		Aws::S3::S3Client& s3Client,
+		const R2Config& config,
+		const string& objectKey);
+
+	void Tick();
+
+	bool IsLoading() const
+	{
+		return m_IsLoading;
+	}
+
+	bool HasResult() const
+	{
+		return m_HasResult;
+	}
+
+	bool IsSuccess() const
+	{
+		return m_HasResult && m_Result.Success;
+	}
+
+	const R2LoadResult& GetResult() const
+	{
+		return m_Result;
+	}
+
+private:
+	bool m_IsLoading = false;
+	bool m_HasResult = false;
+
+	R2LoadResult m_Result;
+	future<R2LoadResult> m_Future;
+};
