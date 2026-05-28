ac4841d0fe5119a55baa5bc2221e623ba6eaa3ec
sydxis <k01023100681@gmail.com>
2026-05-28 11:37:43 +0900

    크래프팅 제작 중

---
+++ TEXT_RPG_5/TEXT_RPG_5/Manager/CraftingManager.cpp
@@ -0,0 +1,202 @@
+﻿//CraftingManager.cpp
+
+#include "CraftingManager.h"
+#include "ItemManager.h"
+#include "../Item/ItemDB.h"
+#include "../Item/Item.h"
+#include "../Component/InventoryComponent.h"
+#include "../Struct/ItemWeight.h"
+
+/* for template
+for (int y = 0; y < Container.size(); ++y)
+{
+    for (int x = 0; x < MaxColumn; ++x)
+    {
+        if (Container[y][x] != nullptr)
+        {
+            >>Execute<<
+        }
+    }
+}
+*/
+
+static const vector<FItemWeight> ShopItemPool =
+{
+    { ItemId::HP_POTION,       150 },
+    { ItemId::STRENGTH_POTION, 50 },
+    { ItemId::LONGSWORD,       10 },
+    { ItemId::BOW,             10 },
+    { ItemId::AXE,             10 },
+    { ItemId::STAFF,           10 },
+    { ItemId::LEATHER_HELMET,   5 },
+    { ItemId::LEATHER_ARMOR,    5 },
+    { ItemId::LEATHER_BOOTS,    5 },
+    { ItemId::PLATE_HELMET,     2 },
+    { ItemId::PLATE_ARMOR,      2 },
+    { ItemId::PLATE_BOOTS,      2 },
+};
+
+CraftingManager::CraftingManager()
+{
+    Container.resize(MaxRow, vector<UItem*>(MaxColumn, nullptr));  // MaxRow 기준으로 초기화
+    RestoreShop();
+}
+
+CraftingManager::~CraftingManager()
+{
+    for (int y = 0; y < Container.size(); ++y)
+    {
+        for (int x = 0; x < MaxColumn; ++x)
+        {
+            delete Container[y][x];
+            Container[y][x] = nullptr;
+        }
+    }
+
+    Container.clear();
+}
+
+
+void CraftingManager::SetPlayerInventory(UInventoryComponent* Inventory)
+{
+    PlayerInventory = Inventory;
+}
+
+void CraftingManager::RestoreShop()
+{
+    for (int y = 0; y < Container.size(); ++y)
+    {
+        for (int x = 0; x < MaxColumn; ++x)
+        {
+            delete Container[y][x];
+        }
+    }
+    
+    Container.clear();
+    Container.resize(MaxRow, vector<UItem*>(MaxColumn, nullptr));  // MaxRow 기준으로 초기화
+
+    for (int i = 0; i < NumberOfItems; ++i)
+    {
+        int x = i % MaxColumn;
+        int y = i / MaxColumn;
+
+        if (y < MaxRow)
+        {
+            Container[y][x] = GetRandomItem();
+        }
+    }
+}
+//테스트
+void CraftingManager::BeginPlay()
+{
+    // ShopPoses.resize(3);
+    //
+    // for (int i = 0 ; i < ShopPoses.size(); ++i)
+    // {
+    //     ShopPoses[i].X = 5 + i * 2;
+    //     ShopPoses[i].Y = 5 + i * 2;
+    // }
+}
+
+
+UItem* CraftingManager::GetItem(Vector Index)
+{
+    if (Index.Y < 0 || Index.Y >= (int)Container.size())
+    {
+        return nullptr;
+    }
+    
+    if (Index.X < 0 || Index.X >= MaxColumn)
+    {
+        return nullptr;
+    }
+    
+    
+    return Container[Index.Y][Index.X];
+}
+
+
+UItem* CraftingManager::GetRandomItem()
+{
+    return ItemManager::GetInstance()->CreateRandomItem(ShopItemPool);
+}
+
+
+bool CraftingManager::SetSellMode(bool EnableSellMode)
+{
+    if (EnableSellMode != bSellMode)
+    {
+        bSellMode = EnableSellMode;
+        if (PlayerInventory != nullptr)
+        {
+            PlayerInventory->ResetCursor();
+        }
+        return bSellMode;
+    }
+
+    return bSellMode;
+}
+
+bool CraftingManager::ToggleSellMode()
+{
+    bSellMode = !bSellMode;
+    if (PlayerInventory != nullptr)
+    {
+        PlayerInventory->ResetCursor();
+    }
+    return bSellMode;
+}
+
+
+
+void CraftingManager::SelectCursor()
+{
+    if (bSellMode == true)
+    {
+        TrySellItem();
+    }
+    else
+    {
+        TryBuyItem();
+    }
+}
+
+void CraftingManager::TryBuyItem()
+{
+    UItem* Item = GetItem(PlayerInventory->GetCursor());
+    if (Item == nullptr)
+    {
+        return;
+    }
+    
+    if (PlayerInventory->GetGold() >= Item->GetItemInfo().Price)
+    {
+        if (PlayerInventory->BuyItem(Item) == true )
+        {
+            Vector Cursor = PlayerInventory->GetCursor();
+            Container[Cursor.Y][Cursor.X] = nullptr;
+        }
+        else
+        {
+            //인벤토리가 가득 찼다는 피드백
+        }
+    }
+    else
+    {
+        //골드가 부족하다는 피드백
+    }
+}
+
+void CraftingManager::EnterNewShop(int ShopId)
+{
+    if (ShopId != LastVisitedShopId)
+    {
+        RestoreShop();
+        LastVisitedShopId = ShopId;
+    }
+}
+
+void CraftingManager::TrySellItem()
+{
+    PlayerInventory->SellItem(PlayerInventory->GetItem(PlayerInventory->GetCursor()));
+}
---
+++ TEXT_RPG_5/TEXT_RPG_5/Manager/CraftingManager.h
@@ -0,0 +1,57 @@
+﻿//CraftingManager.h
+
+#pragma once
+
+#include "../Singleton.h"
+#include "../pch.h"
+#include "../Struct/Vector.h"
+
+
+class UInventoryComponent;
+class UItem;
+
+class CraftingManager : public Singleton<CraftingManager>
+{
+public:
+    
+    CraftingManager();
+    ~CraftingManager();
+    
+private:
+    int NumberOfItems = 8;
+    int MaxColumn = 4;
+    int MaxRow = 4;
+    vector<vector<UItem*>> Container;
+    UInventoryComponent* PlayerInventory = nullptr;
+    Vector CurrentCursor = {0,0};
+    
+    bool bSellMode = false;
+    int LastVisitedShopId = -1;
+
+    vector<Vector> ShopPoses;
+
+    
+public:
+    vector<Vector>& GetShopPoses() { return ShopPoses; }
+    void SetPlayerInventory(UInventoryComponent* Inventory);
+    void RestoreShop();
+    void BeginPlay();
+
+    vector<vector<UItem*>> GetContainer() {return Container;}
+    vector<vector<UItem*>>& GetContainerRef() { return Container; }
+    const vector<vector<UItem*>>& GetContainerRef() const { return Container; }
+    bool GetSellMode() {return bSellMode;}
+    UItem* GetItem(Vector Index);
+    UItem* GetRandomItem();
+    
+    bool SetSellMode(bool EnableSellMode);
+    bool ToggleSellMode();
+    
+    void EnterNewShop(int ShopId);
+
+    void SelectCursor();
+    void TryBuyItem();
+    void TrySellItem();
+
+
+};
